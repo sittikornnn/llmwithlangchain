c9c3dc2aa5fc93149df59996eab3d6e10a8e5310
sittikornnn <115983121+sittikornnn@users.noreply.github.com>
2025-05-21 15:50:49 +0700

    update url for product

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("chatbot.urls")),
+    path("product/", include("product.urls")),
 ]
