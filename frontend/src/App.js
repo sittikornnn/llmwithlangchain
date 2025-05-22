735b8a4be09bccb1af6eddb053ad542563abb8dc
sittikornnn <115983121+sittikornnn@users.noreply.github.com>
2025-05-22 12:23:55 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductPage from "./pages/product";
+import Chatbot from "./pages/chatbot";  // your chatbot component
+import About from "./pages/about";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductPage />} />
+        <Route path="/chat" element={<Chatbot />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
